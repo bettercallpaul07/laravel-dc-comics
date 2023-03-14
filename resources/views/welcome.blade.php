67fe444351517238620f3967f990371dded9fa0c
Paolo Rapuano <bettercallpaul07@gmail.com>
2023-03-14 15:21:16 +0100

    Milestone 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,13 +16,7 @@
                 <div class="row">
                     <div class="col text-center">
 
-                        <div class="card">
-                            <div class="card-body">
-                                <img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
 
-                                <h1 class="card-title">Laravel 9 + Bootstrap Template</h1>
-                            </div>
-                        </div>
 
                     </div>
                 </div>
